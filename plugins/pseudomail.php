92e0da442b2ddf26c2a24560e2f5e56cee0c374c
Stephen Billard <stephen@sbillard.org>
2012-10-03 16:58:49 -0700

    Initial population
    Populate the repository with the Zenphoto Development tools library

---
+++ plugins/pseudomail.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Pseudo mailing handler for localhost testing
+ *
+ * A "mail" file named by the <i>subject</i> is created in the <var>%DATA_FOLDER%</var> folder. Multiple mailings with the
+ * same <i>subject</i> will overwrite.
+ *
+ * @package plugins
+ */
+$plugin_is_filter = 5|CLASS_PLUGIN;
+$plugin_description = gettext("Pseudo mailing handler for localhost testing.");
+$plugin_author = "Stephen Billard (sbillard)";
+$plugin_version = '1.4.3';
+$plugin_disable = (zp_has_filter('sendmail') && !getoption('zp_plugin_pseudomail'))?sprintf(gettext('Only one Email handler plugin may be enalbed. <a href="#%1$s"><code>%1$s</code></a> is already enabled.'),stripSuffix(get_filterScript('sendmail'))):'';
+
+if ($plugin_disable) {
+	setOption('zp_plugin_pseudomail', 0);
+} else {
+	zp_register_filter('sendmail', 'pseudo_sendmail');
+
+}
+
+function pseudo_sendmail($msg, $email_list, $subject, $message, $from_mail, $from_name, $cc_addresses) {
+	$filename = str_replace(array('<', '>', ':', '"'. '/'. '\\', '|', '?', '*'), '_', $subject);
+	$path = SERVERPATH . '/' . DATA_FOLDER . '/'.$filename.'.txt';
+	$f = fopen($path, 'w');
+	fwrite($f,str_pad('*', 49, '-')."\n");
+	$tolist = '';
+	foreach ($email_list as $to) {
+		$tolist .= ','.$to;
+	}
+	fwrite($f, sprintf(gettext('To: %s'),substr($tolist, 1)) . "\n");
+	fwrite($f, sprintf('From: %1$s <%2$s>', $from_name, $from_mail) . "\n");
+	if (count($cc_addresses) > 0) {
+		$cclist = '';
+		foreach ($cc_addresses as $cc_name=>$cc_mail) {
+			$cclist .= ','.$cc_mail;
+		}
+		fwrite($f, sprintf(gettext('Cc: %s'),substr($cclist, 1)) . "\n");
+	}
+	fwrite($f, sprintf(gettext('Subject: %s'),$subject)."\n");
+	fwrite($f,str_pad('*', 49, '-')."\n");
+	fwrite($f, $message . "\n");
+	fclose($f);
+	clearstatcache();
+	return $msg;
+}
+
+?>
---
+++ plugins/zenphoto_package/zenphoto_package_generator.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Zenphoto package list generator
+ *
+ * @author Stephen Billard (sbillard)
+ * @package plugins
+ */
+
+// force UTF-8 Ø
+
+define("OFFSET_PATH",3);
+require_once(dirname(dirname(dirname($_SERVER['SCRIPT_FILENAME'])))."/zp-core/admin-functions.php");
+
+$stdExclude = Array( '.', '..','.DS_Store','.cache','Thumbs.db','.htaccess','.svn','debug.html','.buildpath','.project','.settings');
+$_zp_resident_files[] =	'index.php';
+
+$_zp_resident_files[] = THEMEFOLDER;
+
+$_zp_resident_files[] = THEMEFOLDER.'/default';
+$_zp_resident_files = array_merge($_zp_resident_files,getResidentFiles(SERVERPATH.'/'.THEMEFOLDER.'/default',$stdExclude));
+
+$_zp_resident_files[] = THEMEFOLDER.'/effervescence_plus';
+$_zp_resident_files = array_merge($_zp_resident_files,getResidentFiles(SERVERPATH.'/'.THEMEFOLDER.'/effervescence_plus',$stdExclude));
+
+
+$_zp_resident_files[] = THEMEFOLDER.'/garland';
+$_zp_resident_files = array_merge($_zp_resident_files,getResidentFiles(SERVERPATH.'/'.THEMEFOLDER.'/garland',$stdExclude));
+
+$_zp_resident_files[] = THEMEFOLDER.'/stopdesign';
+$_zp_resident_files = array_merge($_zp_resident_files,getResidentFiles(SERVERPATH.'/'.THEMEFOLDER.'/stopdesign',$stdExclude));
+
+$_zp_resident_files[] = THEMEFOLDER.'/zenpage';
+$_zp_resident_files = array_merge($_zp_resident_files,getResidentFiles(SERVERPATH.'/'.THEMEFOLDER.'/zenpage',$stdExclude));
+
+$_zp_resident_files[] = THEMEFOLDER.'/zpmobile';
+$_zp_resident_files = array_merge($_zp_resident_files,getResidentFiles(SERVERPATH.'/'.THEMEFOLDER.'/zpmobile',$stdExclude));
+
+$_zp_resident_files[] = ZENFOLDER;
+$_zp_resident_files = array_merge($_zp_resident_files,getResidentFiles(SERVERPATH.'/'.ZENFOLDER,$stdExclude));
+
+natsort($_zp_resident_files);
+$filepath = SERVERPATH.'/'.getOption('zenphoto_package_path').'/Zenphoto.package';
+@chmod($filepath, 0666);
+$fp = fopen($filepath, 'w');
+foreach ($_zp_resident_files as $component) {
+	fwrite($fp,$component."\n");
+}
+fwrite($fp,count($_zp_resident_files));
+fclose($fp);
+clearstatcache();
+header('Location: '.FULLWEBPATH.'/'.ZENFOLDER.'/admin.php?action=external&msg=Zenphoto package created and stored in the '.getOption('zenphoto_package_path').' folder.');
+exitZP();
+
+/**
+ *
+ * enumerates the files in folder(s)
+ * @param $folder
+ */
+function getResidentFiles($folder,$exclude) {
+	global $_zp_resident_files;
+	$dirs = array_diff(scandir($folder),$exclude);
+	$localfiles = array();
+	$localfolders = array();
+	foreach($dirs as $file) {
+		$file = str_replace('\\','/',$file);
+		$key = str_replace(SERVERPATH.'/', '', filesystemToInternal($folder.'/'.$file));
+		if (is_dir($folder.'/'.$file)) {
+			$localfolders[] = $key;
+			$localfolders = array_merge($localfolders, getResidentFiles($folder.'/'.$file,$exclude));
+		} else {
+			$localfiles[] = $key;
+		}
+	}
+	return  array_merge($localfiles,$localfolders);
+}
+
+?>
