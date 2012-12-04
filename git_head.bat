97b6918052ebd8c131ecd2c75c77fd758181dd9f
Stephen Billard <stephen@sbillard.org>
2012-12-04 14:58:03 -0800

    make scripts behave more like Mac ones

--- git_head.bat
+++ git_head.bat
@@ -1,5 +1,4 @@
 @echo off
-del zp-core\githead
-git rev-parse HEAD >>zp-core\githead
+git rev-parse HEAD>zp-core\githead
 git add zp-core\githead
 git commit -m"release id"
--- tag.bat
+++ tag.bat
@@ -2,19 +2,17 @@
 SET VERSION=%1
 SET BETAREL=%2
 IF NOT [%VERSION%] == [] GOTO SKIPVERSION
-SET /p VERSION= Enter the Zenphoto version:
+	echo Usage: tag.sh VERSION_NUMBER BETA
+  echo Where BETA is true if the release is beta, empty if not.
+  GOTO END
 :SKIPVERSION
 SET REL=%VERSION%
-IF NOT [%BETAREL%]==[] GOTO ISBETA
-CHOICE /C yn /M "Is this a beta tag?
-IF %ERRORLEVEL% == 2 GOTO TAG
-:ISBETA
+IF [%BETAREL%]==[] GOTO TAG
 SET REL=%VERSION%Beta
-echo v:%REL%
 SET VERSION=%VERSION% Beta
 :TAG
-echo verson:%VERSION%
-echo REL:%REL%
+echo "Tagging %VERSION% (REL=%REL%)..."
 @ECHO ON
 git tag -a -m"Zenphoto version %VERSION%" Zenphoto-%REL%
 git push --tags
+:END
