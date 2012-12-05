bbcdc8d48108786572b0b223154444d355e156d8
Stephen Billard <stephen@sbillard.org>
2012-12-04 16:42:04 -0800

    add release id to commit description

--- git_head.bat
+++ git_head.bat
@@ -1,4 +1,5 @@
 @echo off
 git rev-parse HEAD>zp-core\githead
+FOR /F "tokens=*" %%i in ('git rev-parse --short HEAD') do SET SHORT=%%i 
 git add zp-core\githead
-git commit -m"release id"
+git commit -m"release id %SHORT%"
