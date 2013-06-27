dc3ce6f2b3c585e8c89a0b3d208fed82ad48b21e
acrylian <wartgart@gmx.net>
2013-06-27 19:29:33 +0200

    HTML5 doctype for demo theme files to match official themes/recommendation

--- demo_plugin-and-theme/demo_theme-zenpage-cms-plugin-extras-files/news.php
+++ demo_plugin-and-theme/demo_theme-zenpage-cms-plugin-extras-files/news.php
@@ -2,8 +2,8 @@
 	// force UTF-8 
 	if (!defined('WEBPATH')) die();
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 	<head>
 		<title><?php echo gettext("News"); ?> <?php echo getBareNewsTitle(""); ?><?php printCurrentNewsCategory(" | "); printCurrentNewsArchive(); ?> | <?php echo getBareGalleryTitle(); ?></title>
 		<meta http-equiv="content-type" content="text/html; charset=<?php echo LOCAL_CHARSET; ?>" />
