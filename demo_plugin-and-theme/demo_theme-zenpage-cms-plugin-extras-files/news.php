7d687b862eee61555b6762b64d2b984a36fcce4b
acrylian <wartgart@gmx.net>
2015-08-15 11:39:31 +0200

    In HTML5 meta charset should be first <head> entry

--- demo_plugin-and-theme/demo_theme-zenpage-cms-plugin-extras-files/news.php
+++ demo_plugin-and-theme/demo_theme-zenpage-cms-plugin-extras-files/news.php
@@ -8,8 +8,8 @@
 		<!DOCTYPE html>
 		<html>
 			<head>
-				<?php printHeadTitle(); ?>
 				<meta charset="<?php echo LOCAL_CHARSET; ?>">
+				<?php printHeadTitle(); ?>
 				<link rel="stylesheet" href="<?php echo $_zp_themeroot; ?>/style.css" type="text/css">
 				<?php printZenpageRSSHeaderLink("News","", "Zenpage news", ""); ?>
 				<?php zp_apply_filter('theme_head'); ?>
