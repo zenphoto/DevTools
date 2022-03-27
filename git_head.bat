036d35f6b5e932e47e7c2c6ddcc31d83a1d204a1
acrylian <wartgart@gmx.net>
2022-03-27 19:05:14 +0200

    Remove some unnecessary git files

--- git_head.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-git rev-parse HEAD>zp-core\githead
-SET /P LONG=<zp-core\githead
-SET SHORT=%LONG:~0,10%
-git add zp-core\githead
-git commit -m"release id %SHORT%"
--- git_head.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# No need to remove old file if we use overwrite (single >)
-git rev-parse HEAD > zp-core/githead
-SHORT="$(git rev-parse --short HEAD)"
-git add zp-core/githead
-git commit -m "release id ${SHORT}"
