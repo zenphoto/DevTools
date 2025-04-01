6f2eaa51bcb2039c6d12bc8c84ae566c8ee62882
acrylian <wartgart@gmx.net>
2025-04-01 14:32:03 +0200

    pseudomail: Write mails to pseudomail.log instead individual files

--- plugins/pseudomail/pseudomail.php
+++ plugins/pseudomail/pseudomail.php
@@ -22,33 +22,26 @@ if ($plugin_disable) {
 }
 
 function pseudo_sendmail($msg, $email_list, $subject, $message, $from_mail, $from_name, $cc_addresses, $replyTo) {
-	$filename = str_replace(array('<', '>', ':', '"' . '/' . '\\', '|', '?', '*'), '_', $subject);
-	$path = SERVERPATH . '/' . DATA_FOLDER . '/' . $filename . '.txt';
-	$f = fopen($path, 'w');
-	fwrite($f, str_pad('*', 49, '-') . "\n");
 	$tolist = '';
 	foreach ($email_list as $to) {
 		$tolist .= ',' . $to;
 	}
-	fwrite($f, sprintf(gettext('To: %s'), substr($tolist, 1)) . "\n");
-	fwrite($f, sprintf('From: %1$s <%2$s>', $from_name, $from_mail) . "\n");
+	$content = sprintf(gettext('To: %s'), substr($tolist, 1)) . "\n";
+	$content .= sprintf('From: %1$s <%2$s>', $from_name, $from_mail) . "\n";
 	if ($replyTo) {
 		$names = array_keys($replyTo);
-		fwrite($f, sprintf('Reply-To: %1$s <%2$s>', array_shift($names), array_shift($replyTo)) . "\n");
+		$content .= sprintf('Reply-To: %1$s <%2$s>', array_shift($names), array_shift($replyTo)) . "\n";
 	}
 	if (count($cc_addresses) > 0) {
 		$cclist = '';
 		foreach ($cc_addresses as $cc_name => $cc_mail) {
 			$cclist .= ',' . $cc_mail;
 		}
-		fwrite($f, sprintf(gettext('Cc: %s'), substr($cclist, 1)) . "\n");
+		$content .= sprintf(gettext('Cc: %s'), substr($cclist, 1)) . "\n";
 	}
-	fwrite($f, sprintf(gettext('Subject: %s'), $subject) . "\n");
-	fwrite($f, str_pad('*', 49, '-') . "\n");
-	fwrite($f, $message . "\n");
-	fclose($f);
-	clearstatcache();
+	$content .= sprintf(gettext('Subject: %s'), $subject) . "\n";
+	$content .= $message . "\n";
+	$content .= str_pad('*', 49, '-') . "\n";
+	debuglog(trim($content), false, 'pseudomail');
 	return $msg;
 }
-
-?>
